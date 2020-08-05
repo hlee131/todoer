1738a5d8936163741304d9af036309dffbe6ed2d
Hansel Lee <mrhansellee@gmail.com>
2020-08-05 11:33:04 -0700

    Purged CSS

--- postcss.config.js
+++ postcss.config.js
@@ -3,14 +3,15 @@ module.exports = {
   plugins: [
     tailwindcss("./todoproject/frontend/src/tailwind/tailwind.js"),
     require("autoprefixer"),
-    process.env.NODE_ENV === "production" &&
-      require("@fullhuman/postcss-purgecss")({
-        content: [
-          "./todoproject/frontend/templates/frontend/index.html",
-          "./todoproject/frontend/src/components/**/*.jsx",
-          "./todoproject/frontend/src/components/App.jsx",
-        ],
-        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      }),
+    require("@fullhuman/postcss-purgecss")({
+      content: [
+        "./todoproject/frontend/templates/frontend/index.html",
+        "./todoproject/frontend/src/components/**/*.jsx",
+        "./todoproject/frontend/src/components/App.jsx",
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
   ],
 };
+
+console.log(process.env.NODE_ENV);
